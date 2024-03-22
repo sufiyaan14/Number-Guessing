995136ff9a5decc557451f5535e7f90c492f226f
Sufiyaan Hussain <154407851+sufiyaan14@users.noreply.github.com>
2024-03-23 03:35:03 +0500

    Number Guessing Game

---
+++ index.js
@@ -0,0 +1,17 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+const randomnumber = Math.floor(Math.random() * 6 + 1);
+const answer = await inquirer.prompt([
+    {
+        name: "userGuessedNumber",
+        type: "number",
+        message: "Please guess a number between 1-6",
+    },
+]);
+console.log(answer);
+if (answer.userGuessedNumber === randomnumber) {
+    console.log("Congratulations you guessed right number");
+}
+else {
+    console.log("You guessed wrong number");
+}
